b65f024d5774315fc48fb272c72d010cb85ca583
Mister1SI <wazzup2690@gmail.com>
2025-12-10 20:13:13 -0600

    Add tone changing functionality

--- main.py
+++ main.py
@@ -25,15 +25,19 @@ BG = (30, 30, 30)
 # Create the screen
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Guitar Chord Soundboard")
-font = pygame.font.SysFont(None, 28)
 
-# The default directory for sounds - the 6-string acoustic
-samples_dir = "samples/acoustic_6"
+# Setup the default tone setting
+# 0: ac6
+# 1: ac12
+# 2: el_st
+tone = 0
+tone_paths = ["samples/acoustic_6", "samples/acoustic_12", "samples/electric_strat"]
 
 # Whether to sustain or stop sounds on keyup
 sustain = False
 
 # Button class
+button_font = pygame.font.SysFont(None, 28)
 class Button:
     def __init__(self, x, y, w, h, keyname, label, sound):
         self.rect = pygame.Rect(x, y, w, h)
@@ -58,7 +62,7 @@ class Button:
         pygame.draw.rect(surface, color, self.rect, border_radius=10)
 
         # Draw label text
-        text = font.render(self.label, True, (255, 255, 255))   # Get a pygame Surface
+        text = button_font.render(self.label, True, (255, 255, 255))   # Get a pygame Surface
         # Get the Surface's rect and position it in the center of the related button
         text_rect = text.get_rect(center=self.rect.center)
         surface.blit(text, text_rect)       # Draw
@@ -70,12 +74,12 @@ button_keys = ["q", "w", "e", "r", "t"]
 def load_buttons():
     buttons = []    # Create buttons
     # Search samples/ and filter out only the .wav files
-    files = [f for f in os.listdir(samples_dir) if f.lower().endswith(".wav")]
+    files = [f for f in os.listdir(tone_paths[tone]) if f.lower().endswith(".wav")]
 
     # Go through each file and create its button
     for idx, filename in enumerate(sorted(files)):
         label = os.path.splitext(filename)[0]       # Remove the file extension
-        path = os.path.join(samples_dir, filename)  # Get the path of the sample sound
+        path = os.path.join(tone_paths[tone], filename)  # Get the path of the sample sound
         sound = pygame.mixer.Sound(path)            # Create the Sound object
 
         # Grid layout: 5 per row
@@ -88,10 +92,36 @@ def load_buttons():
         btn = Button(x, y, BTN_W, BTN_H, button_keys[idx], label, sound)
         buttons.append(btn)
 
-    print(files) # DBG print
     return buttons
 
+# Function to draw labels
+subtitle_font = pygame.font.SysFont(None, 24)
+title_font = pygame.font.SysFont(None, 36)
+def draw_labels(surface):
+    # "Tone" label
+    tone_title_text = subtitle_font.render("Tone - Cycle with backspace", True, (200, 200, 200))
+    tone_title_rect = tone_title_text.get_rect(center=(WINDOW_WIDTH/2, 30))
+    surface.blit(tone_title_text, tone_title_rect)
+
+    # The actual selected tone
+    tone_name = ""
+    match tone:
+        case 0:
+            tone_name = "1. Acoustic 6-String"
+        case 1:
+            tone_name = "2. Acoustic 12-String"
+        case 2:
+            tone_name = "3. Fender Stratocaster"
+        case _:
+            tone_name = "undefined"
+
+    tone_text = title_font.render(tone_name, True, (255, 255, 255))
+    tone_rect = tone_text.get_rect(center=(WINDOW_WIDTH/2, 70))
+    surface.blit(tone_text, tone_rect)
+    
+
 buttons = load_buttons()
+draw_labels(screen)
 running = True
 while running:
     # Get the EventList
@@ -105,6 +135,11 @@ while running:
                 # Exit on pressing <Esc>
                 if event.key == pygame.K_ESCAPE:
                     running = False
+                # Cycle tone on backspace
+                if event.key == pygame.K_BACKSPACE:
+                    if tone == 2: tone = 0
+                    else: tone += 1
+                    buttons = load_buttons()
                 # Sustain on on pressing space
                 if event.key == pygame.K_SPACE:
                     sustain = True
@@ -126,6 +161,7 @@ while running:
     screen.fill((20, 20, 20))
     for b in buttons:
         b.draw(screen)
+    draw_labels(screen)
 
     pygame.display.flip()
 
